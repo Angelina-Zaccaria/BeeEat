be705259e813f5f60cf8087bb58b3f09bf312d87
Brittany DiFede <bcd2998@gmail.com>
2020-03-24 17:01:58 -0400

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { TakeComponent } from './take/take.component';
+import { MakeComponent } from './make/make.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TakeComponent,
+    MakeComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
