1b381765b0914c72d6a1d0480323fc1deecaf8cf
Olivia Powers <powersog@gmail.com>
2020-03-25 17:45:12 -0400

    Editing Homepage and Nav Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,9 @@ import { TakeComponent } from './take/take.component';
 import { MakeComponent } from './make/make.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { MaterialModule } from './material/material.module';
+import { MakeQuizComponent } from './make-quiz/make-quiz.component';
+import { MakeSurveyComponent } from './make-survey/make-survey.component';
+
 
 @NgModule({
   declarations: [
@@ -16,13 +20,16 @@ import { MaterialModule } from './material/material.module';
     HomeComponent,
     TakeComponent,
     MakeComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    MakeQuizComponent,
+    MakeSurveyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
