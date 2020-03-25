3afad6aade932cb04464fa037fc801a89eacae75
Olivia Powers <powersog@gmail.com>
2020-03-25 17:52:11 -0400

    Updated Material Module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -13,12 +16,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     CommonModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTabsModule,
+    MatInputModule,
+    MatCardModule
   ],
   exports: [
     MatButtonModule,
     MatFormFieldModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTabsModule,
+    MatInputModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
