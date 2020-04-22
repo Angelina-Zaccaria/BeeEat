b3cfd1be8a83f2db8ea4f610bab748422f150f82
Olivia Powers <powersog@gmail.com>
2020-04-22 00:50:23 -0400

    created take quiz component

---
+++ src/app/take-quiz/take-quiz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-take-quiz',
+  templateUrl: './take-quiz.component.html',
+  styleUrls: ['./take-quiz.component.scss']
+})
+export class TakeQuizComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
