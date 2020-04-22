d82023d3a09a8cc4c38e66b113a99b03ceea15ba
Olivia Powers <powersog@gmail.com>
2020-04-22 00:50:07 -0400

    created take survey component

---
+++ src/app/take-survey/take-survey.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-take-survey',
+  templateUrl: './take-survey.component.html',
+  styleUrls: ['./take-survey.component.scss']
+})
+export class TakeSurveyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
