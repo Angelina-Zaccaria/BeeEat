5b4b27a4046ec291ef3c262ba19985bad8098ce8
Brittany DiFede <bcd2998@gmail.com>
2020-04-23 01:03:11 -0400

    Take survey displaying th surevys from database
    Co-authored-by: olivepowers <powersog@gmail.com>
    Co-authored-by: stevereilly99 <Steven.reilly@yale.edu>
    Co-authored-by: Robert Schaedler III <robertschaedler3@gmail.com>

--- src/app/take-survey/take-survey.component.ts
+++ src/app/take-survey/take-survey.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { DbService } from '../services/db.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-take-survey',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TakeSurveyComponent implements OnInit {
 
-  constructor() { }
+  userInput: FormGroup;
+  public surveyList$: Observable<any[]>;
+  selected: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private afs: AngularFirestore) {
+    this.surveyList$ = this.afs.collection('/survey').valueChanges({ idField: 'id' });
+  }
 
   ngOnInit(): void {
+    // this.userInput = this.fb.group({})
+    console.log()
+  }
+
+  select(survey) {
+    this.selected = survey;
+    let groupData = {}
+    for (let i = 0; i < this.selected.questions.length; i++) {
+      // groupData[`${i}`] = ['', Validators.required];
+      groupData[`${i}`] = '';
+    }
+    this.userInput = this.fb.group(groupData);
+    console.log(groupData);
+  }
+
+  clear() {
+    this.selected = null;
+  }
+
+  submit() {
+    const response = this.userInput.value;
+    this.afs.doc(`quiz/${this.selected.id}`).collection('answers').add(response).then(() => {
+      // TODO: navigate to new page
+    })
   }
 
 }
+
+
