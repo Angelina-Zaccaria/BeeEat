f9dcbaff0a1a3be48ec27f2af933e53e87c1b625
Brittany DiFede <bcd2998@gmail.com>
2020-04-23 01:14:15 -0400

    take quiz ts

--- src/app/take-quiz/take-quiz.component.ts
+++ src/app/take-quiz/take-quiz.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { DbService } from '../services/db.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, } from 'rxjs/internal/Observable';
+// class Quiz {
+//   constructor(public title) { }
+// }
 
 @Component({
   selector: 'app-take-quiz',
@@ -7,9 +14,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TakeQuizComponent implements OnInit {
 
-  constructor() { }
+  public quizList$: Observable<any[]>;
+  selected: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private afs: AngularFirestore) {
+    this.quizList$ = this.afs.collection('/quiz').valueChanges({ idField: 'id' });
+  }
+
 
   ngOnInit(): void {
   }
+
+  select(quiz) {
+    this.selected = quiz;
+  }
+
+  clear() {
+    this.selected = null;
+  }
+
 
 }
