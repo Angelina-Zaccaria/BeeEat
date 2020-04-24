2dbb3e59cbdc133f0cd695f1f13b1a530c4dcbd7
Olivia Powers <powersog@gmail.com>
2020-04-24 00:55:56 -0400

    preliminary answer checking
    Co-authored-by: Angelina Zaccaria <angelina.zaccaria@gmail.com>
    Co-authored-by: bcd2998 <bcd2998@gmail.com>

--- src/app/take-survey/take-survey.component.ts
+++ src/app/take-survey/take-survey.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { DbService } from '../services/db.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs/internal/Observable';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-take-survey',
@@ -11,13 +12,16 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class TakeSurveyComponent implements OnInit {
 
+
   userInput: FormGroup;
   public surveyList$: Observable<any[]>;
   selected: any;
+  submitClicked: boolean = false;
 
   constructor(
     private fb: FormBuilder,
-    private afs: AngularFirestore) {
+    private afs: AngularFirestore,
+    private router: Router) {
     this.surveyList$ = this.afs.collection('/survey').valueChanges({ idField: 'id' });
   }
 
@@ -25,27 +29,40 @@ export class TakeSurveyComponent implements OnInit {
     // this.userInput = this.fb.group({})
     console.log()
   }
-
+  
   select(survey) {
-    this.selected = survey;
     let groupData = {}
-    for (let i = 0; i < this.selected.questions.length; i++) {
+    for (let i = 0; i < survey.questions.length; i++) {
       // groupData[`${i}`] = ['', Validators.required];
       groupData[`${i}`] = '';
     }
     this.userInput = this.fb.group(groupData);
+    this.userInput.valueChanges.subscribe(console.log);
+    this.selected = survey; 
     console.log(groupData);
   }
+
+  // totalUsers() {
+  //   const db = firebase.firestore();
+  //   const increment = firebase.firestore.FieldValue.increment(1);
+
+  //   const survey = db.collection('survey').doc(`${this.selected.id}`)
+  // }
+
 
   clear() {
     this.selected = null;
   }
 
   submit() {
+    const title = this.selected.title;
     const response = this.userInput.value;
-    this.afs.doc(`quiz/${this.selected.id}`).collection('answers').add(response).then(() => {
+    this.afs.doc(`survey/${this.selected.id}`).collection('answers').add(response).then(() => {
       // TODO: navigate to new page
+      // this.router.navigate(['/results', title])
+      // , {state: {userInput: this.userInput, data: this.selected}}
     })
+    this.submitClicked = !this.submitClicked;
   }
 
 }
