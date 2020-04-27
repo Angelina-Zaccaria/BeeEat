8883422a842b1062e016de863e7dec0cbc75b43c
Olivia Powers <powersog@gmail.com>
2020-04-27 19:57:05 -0400

    delete saved answers from db with edit
    Co-authored-by: Angelina Zaccaria <angelina.zaccaria@gmail.com>
    Co-authored-by: bcd2998 <bcd2998@gmail.com>

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 
+import { firestore } from 'firebase';
+
 
 @Injectable({
   providedIn: 'root'
@@ -20,9 +22,26 @@ export class DbService {
     return editKey;
   }
 
+
+  deleteCollection(collection, editKey) {
+    this.afs.collection(`${collection}/${editKey}/answers`).get().subscribe(result => {
+      result.docs.forEach(doc => {
+        this.afs.doc(`${collection}/${editKey}/answers/${doc.id}`).delete();
+      })
+    });
+
+  }
+
   async update(collection: 'quiz' | 'survey', data: any, editKey: string) {
     console.log(editKey)
+    editKey = editKey.replace(' ', '');
     await this.afs.doc(`${collection}/${editKey}`).set(data);
+    if(collection == 'survey'){
+      await this.afs.doc(`${collection}/${editKey}`).update({
+        count: 0
+      });
+    }
+    this.deleteCollection(collection, editKey)
     this.router.navigate(['/success', editKey])
 
     return editKey;
