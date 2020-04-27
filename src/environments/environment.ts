11aca9fc0fd788627efcf61ed023a1a17f65eb99
Olivia Powers <powersog@gmail.com>
2020-04-27 01:50:26 -0400

    fixing environment... leave as is
    Co-authored-by: bcd2998 <bcd2998@gmail.com>

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    // Your web app's Firebase configuration
+    firebaseConfig: {
+        apiKey: "AIzaSyDvdi_-qSBX2XktW4-FooR8a46igiXjiZg",
+        authDomain: "beeeat-cbb49.firebaseapp.com",
+        databaseURL: "https://beeeat-cbb49.firebaseio.com",
+        projectId: "beeeat-cbb49",
+        storageBucket: "beeeat-cbb49.appspot.com",
+        messagingSenderId: "365600460094",
+        appId: "1:365600460094:web:d6f48de26751e4f76af06f",
+        measurementId: "G-48TXEHYRBV"
+    }
+}
